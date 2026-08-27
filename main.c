4b8dbc29693c732f19829a951a5d12a22fc39b2c
eugen vrlic <eugen.vrlic@skilja.com>
2026-08-27 20:51:20 +0200

    Add function  to draw Circle for exercise. Initial setup for Triangle function, which will later serve as main drawing function for each 2D&3D model.

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <Windows.h>
+#include <math.h>
 
 WINDOW_HEIGHT = 500;
 WINDOW_WIDTH = 500;
@@ -9,6 +10,17 @@ typedef struct {
 	int x;
 	int y;
 } xyStart;
+
+typedef struct {
+	int x;
+	int y;
+}Point2D;
+
+typedef struct {
+	float x;
+	float y;
+	float z;
+}Point3D;
 
 const char windowClassName[] = "CubeWindow";
 uint32_t* pixel_buffer;
@@ -53,7 +65,40 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 	return 0;
 }
 
-void DrawRectangle(xyStart sCoord, int w, int h, uint32_t color)
+void swap_points2d(Point2D* point1, Point2D*  point2)
+{
+	Point2D temp = *point1;
+	*point1 = *point2;
+	*point2 = temp;
+}
+
+void DrawTriangle(Point2D point1, Point2D point2, Point2D point3)
+{
+	Point2D vertice_top;
+	Point2D vertice_middle;
+	Point2D vertice_bottom;
+
+	vertice_top= point1;
+	vertice_middle = point2;
+	vertice_bottom = point3;
+
+	if (vertice_top.y < vertice_middle.y)
+	{
+		swap_points2d(&vertice_top,&vertice_middle);
+	}
+	if (vertice_top.y < vertice_bottom.y )
+	{
+		swap_points2d(&vertice_top, &vertice_bottom);
+	}
+	if (vertice_middle.y < vertice_bottom.y)
+	{
+		swap_points2d(&vertice_middle, &vertice_bottom);
+	}
+
+	//int delta_x1 = 
+}
+
+void DrawPrimitiveRectangle(xyStart sCoord, int w, int h, uint32_t color)
 {
 	for (int py = sCoord.y; py < sCoord.y + h; py++)
 	{
@@ -65,19 +110,52 @@ void DrawRectangle(xyStart sCoord, int w, int h, uint32_t color)
 	}
 }
 
+
+void DrawCircle(xyStart sCoord, int radius, uint32_t color)
+{
+	int x_distance = 0;
+	int y_distance = 0;
+	double point_distance=0.0;
+	for (int py = 0; py < WINDOW_HEIGHT; py++)
+	{
+		for (int px = 0; px < WINDOW_WIDTH; px++)
+		{
+			x_distance = abs(sCoord.x - px);
+			y_distance = abs(sCoord.y - py);
+			point_distance = (double)sqrt(x_distance*x_distance + y_distance*y_distance);
+			if (point_distance <= radius)
+			{
+				int i = py * WINDOW_WIDTH + px;
+				pixel_buffer[i] = color;
+			}
+		}
+	}
+}
+
+
 int main()
 {
 	printf("Hello there");
 	
-	int rect_w = 100;
-	int rect_h = 100;
+	int rect_w = 50;
+	int rect_h = 50;
 	xyStart sCoord = { 100 ,100 };
+	xyStart sCoordCircle = { 200 ,200 };
 
 	// Window buffer
 	pixel_buffer = malloc(WINDOW_HEIGHT * WINDOW_WIDTH * sizeof(uint32_t));
 	uint32_t color = 0x00FF0000; // Red
-	DrawRectangle(sCoord, rect_w, rect_h, color);
+	
+	// Draw
+	//DrawPrimitiveRectangle(sCoord, rect_w, rect_h, color);
+	//DrawCircle(sCoordCircle, 50, color);
 
+
+	Point2D xy1 = {1,1};
+	Point2D xy2 = {2,2};
+	Point2D xy3 = {3,3};
+	DrawTriangle(xy1, xy2, xy3);
+	
 	// Bitmap
 	bmpi.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
 	bmpi.bmiHeader.biWidth = WINDOW_WIDTH;
@@ -113,7 +191,7 @@ int main()
 	HWND hwnd;
 	hwnd = CreateWindowEx(WS_EX_CLIENTEDGE,
 		windowClassName,
-		L"Cube window",
+		L"CCube",
 		WS_OVERLAPPEDWINDOW,
 		CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT,
 		NULL, NULL, GetModuleHandle(NULL), NULL);
@@ -124,7 +202,6 @@ int main()
 	}
 	ShowWindow(hwnd, SW_SHOWNORMAL);
 	UpdateWindow(hwnd);
-
 
 	while (GetMessage(&Msg, NULL, 0, 0) > 0)
 	{
